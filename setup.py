511d7019a9cc055ec387e497be3757dbb2ac6edc
Søren Fuglede Jørgensen <s@fuglede.dk>
2023-04-10 21:32:17 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.rst", "rb") as f:
 
 setuptools.setup(
     name="burau",
-    version="0.0.3",
+    version="0.0.4",
     author="Søren Fuglede Jørgensen",
     author_email="pypi@fuglede.dk",
     description="Search for non-trivial elements of the kernel of the Burau "
